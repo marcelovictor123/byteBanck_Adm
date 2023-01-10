1d27ac8c5192e16f4927608dc9261cdb5c51504d
marcelo_victor <marcelo.victor525@gmail.com>
2023-01-09 23:10:34 -0300

    Adicionando class Funcionario

---
+++ byteBanck_Adm/Program.cs
@@ -0,0 +1,9 @@
+﻿using byteBanck_Adm.Funcionarios;
+
+Funcionario Pedro = new Funcionario();
+Pedro.Nome = "Pedro andré";
+Pedro.Cpf = "12345678911";
+Pedro.Salario = 2000;
+
+Console.WriteLine(Pedro.Nome);
+Console.WriteLine(Pedro.GetBonificacao());
