e1c06bffbc2c09873266f37f935f2db3b7e6ef15
marcelo_victor <marcelo.victor525@gmail.com>
2023-01-18 23:03:25 -0300

    Adicionando class utilitario

---
+++ byteBanck_Adm/Parceria/ParceiroComercial.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace byteBanck_Adm.Parceria
+{
+    public class ParceiroComercial
+    {
+        public string Senha { get; set }
+    }
+    public override bool Autenticar(string senha)
+    {
+        return this.Senha == senha;
+    }
+}
